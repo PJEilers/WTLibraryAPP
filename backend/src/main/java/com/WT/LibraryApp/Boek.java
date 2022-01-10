8bc649c81ea9b4d892ff60ae04982b00793242cf
Dylan Kossen <dylan.kossen@hotmail.com>
2022-01-10 10:52:00 +0100

    created vars, getters, setters in boek.java

--- backend/src/main/java/com/WT/LibraryApp/Boek.java
+++ backend/src/main/java/com/WT/LibraryApp/Boek.java
@@ -1,5 +1,93 @@
 package com.WT.LibraryApp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "boek")
 public class Boek {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 100, nullable = false)
+	private String titel;
+	
+	@Column(length = 100, nullable = false)
+	private String auteur;
+	
+	@Column(length = 50, nullable = false)
+	private String isbn;
+	
+	@Column(length = 20)
+	private String druk;
+	
+	@Column(length = 1000)
+	private String beschrijving;
+	
+	@Column(length = 500)
+	private String tags;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getDruk() {
+		return druk;
+	}
+
+	public void setDruk(String druk) {
+		this.druk = druk;
+	}
+
+	public String getBeschrijving() {
+		return beschrijving;
+	}
+
+	public void setBeschrijving(String beschrijving) {
+		this.beschrijving = beschrijving;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	
+	
 }
