fca924fe324256eda3d3b4e9dc8cdcf9501e4987
PJEilers <p.j.eilers@student.rug.nl>
2022-01-11 10:58:24 +0100

    Persoon repo, service, controller aangemaakt met een aantal functionaliteiten

---
+++ backend/src/main/java/com/WT/LibraryApp/Persoon/Persoon.java
@@ -0,0 +1,72 @@
+package com.WT.LibraryApp.Persoon;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persoon")
+public class Persoon {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(length = 100, nullable = false)
+	private String naam;
+
+	@Column(unique = true, length = 100, nullable = false)
+	private String email;
+
+    @Column(length = 100, nullable = false)
+    private String wachtwoord;
+
+    @Column
+    private Boolean adminRechten = false;
+
+
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+
+    public Boolean getAdminRechten() {
+        return adminRechten;
+    }
+
+    public void setAdminRechten(Boolean adminRechten) {
+        this.adminRechten = adminRechten;
+    }
+
+}
+
---
+++ backend/src/main/java/com/WT/LibraryApp/Persoon/PersoonService.java
@@ -0,0 +1,29 @@
+package com.WT.LibraryApp.Persoon;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersoonService {
+    
+    @Autowired
+    private IPersoonRepository repository;
+
+    public Persoon maakPersoonAan(Persoon persoon) {
+        return repository.save(persoon);
+    }
+
+    public List<Persoon> vindAllePersonen () {
+        return repository.findAll();
+    }
+
+    public Optional<Persoon> vindPersoon(int id) {
+        return repository.findById(id);
+    }
+
+
+
+}
