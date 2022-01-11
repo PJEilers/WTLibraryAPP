646e47479040d7132202aaf2c7f9db4369a334d4
PJEilers <pieterjaneilers@gmail.com>
2022-01-11 09:10:15 +0100

    Exemplaren toevoegen (#10)
    * Boek Toevoegen Formulier
    
    * Volledig formulier aangemaakt met verplichte velden
    
    * Beschrijving resizable text-box gemaakt
    
    * Boek Toevoegen Formulier
    
    Volledig formulier aangemaakt met verplichte velden
    
    Beschrijving resizable text-box gemaakt
    
    Druk en Beschrijving weggehaald
    
    * Exemplaren toevoegd, verschijnt nadat je boek hebt toegevoegd
    
    * Knopje nieuw boek toegevoegd
    
    * Kleine bugfix

--- frontend/src/BoekToevoegen.js
+++ frontend/src/BoekToevoegen.js
@@ -1,14 +1,18 @@
 import './BoekToevoegen.css';
 import {useState} from "react";
+import ExemplarenToevoegen from './ExemplarenToevoegen';
 
 function BoekToevoegen() {
 
     const [boektitel, setBoektitel] = useState('');
     const [auteur, setAuteur] = useState('');
-    const [beschrijving, setBeschrijving] = useState('');
-    const [ISBN, setISBN] = useState(null);
-    const [druk, setDruk] = useState(null);
+    const [ISBN, setISBN] = useState('');
     const [tags, setTags] = useState(null);
+    const [alleenLezen, setAlleenLezen] = useState(false)
+    const [knopUit, setKnopUit] = useState(false)
+    const [boekToegevoegd, setBoekToegevoegd] = useState(false)
+    const [uniekID, setUniekID] = useState(0)
+    
 
     const stuurOp = () => {
         if (boektitel == '' || auteur == '' || ISBN == '') {
@@ -16,32 +20,57 @@ function BoekToevoegen() {
         } else {
             const boekData = {}
             //TODO: POST request/response met correct json
+            setAlleenLezen(true)
+            setKnopUit(true)
+            setBoekToegevoegd(true)
+
         }
+    }
 
-
+    const reset = () => {
+        setBoekToegevoegd(false)
+        setAlleenLezen(false)
+        setKnopUit(false)
+        setBoektitel('')
+        setAuteur('')
+        setISBN('')
+        setTags(null)
+        setUniekID(uniekID+1)
     }
 
     return (
-        <div>            
+        <div key = {uniekID}>            
             <h1>Voeg een boek toe</h1>
             <div className='BoekToevoegen'>
 
                 <label>Boektitel</label>
-                <input type="text" placeholder = "verplicht veld" onChange={e => setBoektitel(e.target.value)}/>
+                <input type="text" placeholder = "verplicht veld" 
+                                   onChange={e => setBoektitel(e.target.value)}
+                                   readOnly={alleenLezen}/>
 
                 <label>Auteur</label>
-                <input type="text" placeholder = "verplicht veld"  onChange={e => setAuteur(e.target.value)} />
+                <input type="text" placeholder = "verplicht veld"  
+                                   onChange={e => setAuteur(e.target.value)} 
+                                   readOnly = {alleenLezen}/>
 
                 <label>ISBN</label>
-                <input type="text" placeholder = "verplicht veld"  onChange={e => setISBN(e.target.value)} />
-                
+                <input type="text" placeholder = "verplicht veld"  
+                                   onChange={e => setISBN(e.target.value)} 
+                                   readOnly = {alleenLezen}/>
+
                 <label>Tags</label>
-                <input type="text" placeholder = "gescheiden door komma" onChange={e => setTags(e.target.value)} />   
+                <input type="text" placeholder = "gescheiden door komma" 
+                                   onChange={e => setTags(e.target.value)} 
+                                   readOnly = {alleenLezen}/>   
 
                 <label id = "stuur"></label>
-                <button onClick = {() => stuurOp()}>Maak nieuw boek aan</button>
+                <button disabled = {knopUit} onClick = {() => stuurOp()}>Maak nieuw boek aan</button>
+
 
             </div>          
+            <ExemplarenToevoegen boekToegevoegd={boekToegevoegd} boektitel = {boektitel}
+                                 uniekID={uniekID}/>
+            <button onClick={() => reset()}>Nieuw Boek</button>
         </div>
     )
 }
