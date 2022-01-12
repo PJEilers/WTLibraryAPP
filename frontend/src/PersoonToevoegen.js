3e94f8d3119b430ffa5878acfd187ace976e637f
PJEilers <pieterjaneilers@gmail.com>
2022-01-12 09:48:14 +0100

    Frontend persoon toevoegen incl connectie (#28)
    * Persoon repo, service, controller aangemaakt met een aantal functionaliteiten
    
    * Persoon toevoegen werkt, ben nog bezig met regex voor email
    
    * regex voor email
    
    * velden disabled nadat de persoon is toegevoegd

---
+++ frontend/src/PersoonToevoegen.js
@@ -0,0 +1,92 @@
+import "./PersoonToevoegen.css"
+import {useState} from 'react'
+
+function PersoonToevoegen () {
+
+    const [naam, setNaam] = useState('');
+    const [email, setEmail] = useState('');
+    const [adminRechten, setAdminRechten] = useState(false);
+    const [succesBericht, setSuccesBericht] = useState('');
+    const [alleenLezen, setAlleenLezen] = useState(false);
+    const [knopUit, setKnopUit] = useState(false);
+    const [resetID, setResetID] = useState(0);
+
+
+    const nieuwPersoon = () => {
+        if (!email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) 
+        || naam == '') {
+            alert('Vul een geldig e-mailadres en naam in');
+        } else {
+            const nieuwPersoon = {
+                naam: naam,
+                email: email,
+                wachtwoord: "WT123",
+                adminRechten: adminRechten,
+            };
+            
+            maakNiewPersoonAan("http://localhost:8080/maakpersoonaan", nieuwPersoon).then(response => {
+                if (response.ok) {
+                    setSuccesBericht('Persoon is toegevoegd')
+                    setAlleenLezen(true)
+                    setKnopUit(true)
+                } else {
+                    setSuccesBericht('E-mailadres is niet uniek')
+                }
+            }).catch(error => console.log(error))
+        }       
+    }
+
+
+    const reset = () => {
+        setAdminRechten(false);
+        setEmail('');
+        setNaam('');
+        setSuccesBericht('');
+        setAlleenLezen(false);
+        setKnopUit(false)
+        setResetID(resetID+1);
+    }
+
+
+    return (
+        <div key = {resetID}>
+            <h1>Voeg een persoon toe</h1>
+            <div className = "PersoonToevoegen">
+   
+                <label>Volledige naam</label>
+                <input type = "text" 
+                       readOnly = {alleenLezen}
+                       onChange = {e => setNaam(e.target.value)}/>
+
+                <label>E-mail</label>
+                <input type = "email" 
+                       readOnly = {alleenLezen}
+                       onChange = {e => setEmail(e.target.value)}/>
+
+                <label>Admin rechten</label>
+                <input type = "checkbox" id = "checkbox" disabled={knopUit}
+                onChange ={e => setAdminRechten(e.target.checked)}/>
+
+                <label id = "stuur"></label>
+                <button disabled={knopUit} onClick = {() => nieuwPersoon()}>Maak nieuw persoon aan</button>
+
+            </div>
+            <p>{succesBericht}</p>
+            <button onClick = {() => reset()}>Nog een persoon aanmaken</button>
+
+        </div>
+    )
+}
+
+const maakNiewPersoonAan = async(url, persoon) =>  {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(persoon)
+    })
+    return response;
+}
+
+export default PersoonToevoegen;
