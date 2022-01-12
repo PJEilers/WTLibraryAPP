e096d667bb7cc9d46d130dbade3695c244df39de
DanielW1996 <96585054+DanielW1996@users.noreply.github.com>
2022-01-12 15:21:54 +0100

    Backend uitlening (#36)
    * Uitlening backend, nog niet getest
    
    * Backend uitlening aanmaken werkt

---
+++ backend/src/main/java/com/WT/LibraryApp/Uitlening/IUitleningRepository.java
@@ -0,0 +1,7 @@
+package com.WT.LibraryApp.Uitlening;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUitleningRepository extends JpaRepository<Uitlening, Integer>{
+
+}
