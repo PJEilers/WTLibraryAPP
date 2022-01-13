19660bee1c9c1fb2bd522751da1e51ebdf351e47
PJEilers <p.j.eilers@student.rug.nl>
2022-01-13 12:44:13 +0100

    Form + andere refactorings

--- frontend/src/Constanten.js
+++ frontend/src/Constanten.js
@@ -1,3 +1,1 @@
-export const connectieString = "localhost:8080"
-export const emailCheck = 
-/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+export const connectieString = "localhost:8080";
