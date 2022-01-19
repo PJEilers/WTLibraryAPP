44af690a95bf324d702229b53a1062889621a450
DylanK98 <71122833+DylanK98@users.noreply.github.com>
2022-01-19 09:14:19 +0100

    Dylan/uitleen historie weergave (#82)
    * basic uitleen historie weergeven
    
    * tabel is af, alleen in backend nog de einddatum meegeven zodra dat aangemaakt is

---
+++ frontend/src/components/pages/Reserveringen/UitleenHistorie.js
@@ -0,0 +1,62 @@
+import './UitleenHistorie.css';
+import React from 'react';
+import { useState } from 'react';
+
+function UitleenHistorieTabel() {
+    const [uitleningen, setUitleningen] = useState([]);
+    const [firstBoot, setFirstBoot] = useState(true);
+
+    const uitleenData = () => {
+        fetch('http://localhost:8080/historie', { mode: 'cors' })
+            .then(response => response.json())
+            .then(data => {
+                setUitleningen(data);
+            })
+            .catch(error => {
+                console.error('Error: ', error);
+            })
+    }
+
+    if (firstBoot) {
+        uitleenData();
+        setFirstBoot(false);
+    }
+
+    /*
+    in de backend de juiste info verzamelen in uitleningen controller, en dan als 1 object naar de frontend
+    ipv id uit de exemplaar tabel moet boekid.exemplaarid getoond worden
+        -in exemplaar functie die
+            -id, exemplaarid, boekid geeft
+    ipv persoon id de naam van de persoon tonen
+        -in persoon functie die
+            -id, naam geeft
+    */
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Exemplaar ID</th>
+                        <th>Persoon</th>
+                        <th>Begin Datum</th>
+                        <th>Eind Datum</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {uitleningen.map(uitlening => (
+                        <tr key={uitlening.id}>
+                            <td>{uitlening.id}</td>
+                            <td>WT-{uitlening.boekId}.{uitlening.exemplaarId}</td>
+                            <td>{uitlening.persoon}</td>
+                            <td>{uitlening.beginDatum}</td>
+                            <td>{uitlening.eindDatum}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default UitleenHistorieTabel;
