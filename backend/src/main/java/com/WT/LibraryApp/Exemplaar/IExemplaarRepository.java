d248e7af11e97d3491fa7483b74c0374c8d1d2bc
PJEilers <pieterjaneilers@gmail.com>
2022-01-24 09:26:40 +0100

    In exemplaar een ManyToOne relatie met boek aangemaakt (#110)
    * Generieke Tabe Style toegevoegd + style status
    
    * Footer toegevoegd, inclusief routing naar andere pagina's. De contents van de footer kunnen altijd worden aangepast naar wat echt nodig is.
    
    * ManyToOne geregeld voor exemplaar met boek
    
    * Dependency toegevoegd voor auto update als je iets veranderd
    
    Co-authored-by: Huub V <huubvanderveeken@gmail.com>
    Co-authored-by: Olaf <leenders.olaf@gmail.com>

--- backend/src/main/java/com/WT/LibraryApp/Exemplaar/IExemplaarRepository.java
+++ backend/src/main/java/com/WT/LibraryApp/Exemplaar/IExemplaarRepository.java
@@ -2,13 +2,14 @@ package com.WT.LibraryApp.Exemplaar;
 
 import java.util.List;
 
+import com.WT.LibraryApp.Boek.Boek;
 import com.WT.LibraryApp.Exemplaar.Exemplaar.Status;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IExemplaarRepository extends JpaRepository<Exemplaar, Integer> {
 		List<Exemplaar> findByBoekId(int boekId);
-		int countByBoekId(int boekId);
-		int countByBoekIdAndStatusEquals(int boekId, Status status);
+		int countByBoek(Boek boek);
+		int countByBoekAndStatus(Boek boek, Status status);
 }
 
