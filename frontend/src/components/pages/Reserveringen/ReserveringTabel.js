071516197f3ee33984384c2f35fa2aced730acf3
Dylan Kossen <dylan.kossen@hotmail.com>
2022-01-31 16:03:28 +0100

    reserveringtabel werkt nu ook volledig met pagination

--- frontend/src/components/pages/Reserveringen/ReserveringTabel.js
+++ frontend/src/components/pages/Reserveringen/ReserveringTabel.js
@@ -8,7 +8,6 @@ import { uitleningToevoegen } from '../../../Constanten'
 import ExemplaarInformatie from '../Boeken/ExemplaarInformatie';
 
 import { useTable, usePagination } from "react-table";
-import {BasicTable} from '../../Styling/Pagination.js';
 
 function MaakReserveringTabel() {
     const [reserveringen, setReserveringen] = useState([]);
@@ -41,7 +40,7 @@ function MaakReserveringTabel() {
             {
                 Header: ' ',
                 Cell: ({ cell }) => (
-                    <Button onClick={() => console.log(cell.row.original.persoonId + ", " + cell.row.original.boekId + ", " + cell.row.original.id)}>Uitlenen</Button>
+                    <Button onClick={() => setUitleningInfo(cell.row.original.persoonId, cell.row.original.boekId, cell.row.original.id)}>Uitlenen</Button>
                 )
             },
         ]
@@ -91,12 +90,6 @@ function MaakReserveringTabel() {
         laadData();
     }, [opstarten, nieuweUitlening])
 
-    const zetButtonInData = () => {
-        reserveringWeergeven.forEach(element => {
-            element.Button = <Button onClick={() => alert("test")}>Uitlenen</Button>;
-        })
-    }
-
     return (
         <div>
             <h1 className='paragraph'>
@@ -107,9 +100,97 @@ function MaakReserveringTabel() {
 
             <TableStyle>
                 <BasicTable columns={columns} data={reserveringWeergeven}/>
+                
+                <Popup open={nieuweUitlening} modal onClose={() => setNieuweUitlening(false)}>
+                    <div className="modal">
+                        <button className="close" onClick={() => setNieuweUitlening(false)}> &times; </button>
+                        <ExemplaarInformatie persoon={huidigPersoon} boekId={huidigBoek} reserveringId={huidigReserveringId} />
+                    </div>
+                </Popup>
             </TableStyle>
         </div>
     );
 }
+
+
+//niet optimaal om dit in deze file te zetten
+//maar maakt het een stuk makkelijker met de uitleen knop
+const BasicTable = ({ columns, data }) => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page, // Instead of using 'rows', we'll use page,
+        // which has only the rows for the active page
+    
+        // The rest of these things are super handy, too ;)
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        state: { pageIndex },
+      } = useTable(
+        {
+          columns,
+          data,
+          initialState: { pageSize: 20 },
+        },
+        usePagination
+      )
+
+      // Render the UI for your table
+      return (
+        <>
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row, i) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+
+        <div className="pagination">
+            <Button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+              {'<<'}
+            </Button>{' '}
+            <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+              {'<'}
+            </Button>{' '}
+            <span>
+              Page{' '}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>{' '}
+            </span>
+            <Button onClick={() => nextPage()} disabled={!canNextPage}>
+              {'>'}
+            </Button>{' '}
+            <Button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+              {'>>'}
+            </Button>{' '}
+        </div>
+      </>
+    )
+  }
 
 export default MaakReserveringTabel;
