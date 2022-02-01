1b9a76849c856c32cc55bbf54ab4bdd3e2d2b8d9
OlafGitgud <96584989+OlafGitgud@users.noreply.github.com>
2022-02-01 09:21:56 +0100

    Restyling van de navbar zodat er minder drop down menus nodig zijn. Ook heb ik een pagina gemaakt wanneer je op toevoegen klikt. Je komt dan op een pagina terecht met links. Verder heb ik kleine aanpassingen op de styling van de applicatie gemaakt. (#166)

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,8 +1,24 @@
 import React from 'react';
+
 const Contact = () =>{
   return (
     <div>
-      <h3>Contact gegevens </h3>
+      <h1>Contact gegevens </h1>
+
+      <ul>
+        <li>
+          Working Talent
+        </li>
+        <li>
+          Kerkbuurt 4
+        </li>
+        <li>
+          1551 BD Oostzaan
+        </li>
+        <li>
+          tel: 08506 08506
+        </li>
+      </ul>
     </div>
   );
 }
